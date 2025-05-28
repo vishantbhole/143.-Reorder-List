385bdabead9c18110bbe20a31bb19b52de84d414
Vishant <44126013+vishantbhole@users.noreply.github.com>
2025-05-28 13:59:40 -0400

    list_to_linkedlist

---
+++ reorderList.py
@@ -0,0 +1,11 @@
+# ---------- Test Cases Below ----------
+
+def list_to_linkedlist(arr):
+    if not arr:
+        return None
+    head = ListNode(arr[0])
+    current = head
+    for val in arr[1:]:
+        current.next = ListNode(val)
+        current = current.next
+    return head
